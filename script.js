4264f144bdc14c0d0d0b195a4a262c095cd8204b
Yash Dewan <yashdewan2580@gmail.com>
2023-04-10 16:28:47 +0530

    Commit

--- script.js
+++ script.js
@@ -11,21 +11,91 @@ const arrdict = [
         span: "Women's Lower",
         h5: "Leapord print Night Trouser",
         h4: "Rs 290/-",
+        itemCode: "l1",
+        prod: "womenLower"
     },
     {
         imgsrc: "img/p2.jpg",
         alt: "product2",
         span: "Women's Lower",
-        h5: "Leapord print Night Trouser",
+        h5: "Red Check print Night Trouser",
         h4: "Rs 290/-",
+        itemCode: "c1",
+        prod: "womenLower"
     },
     {
         imgsrc: "img/p (1).jpg",
-        alt: "product2",
+        alt: "product3",
         span: "Women's Lower",
         h5: "Leapord print Night Trouser",
         h4: "Rs 290/-",
+        itemCode: "l2",
+        prod: "womenLower"
+    },
+    {
+        imgsrc: "img/p (2).jpg",
+        alt: "product4",
+        span: "Women's Lower",
+        h5: "Black Check print Night Trouser",
+        h4: "Rs 290/-",
+        itemCode: "c2",
+        prod: "womenLower"
+    },
+    {
+        imgsrc: "img/p (3).jpg",
+        alt: "product5",
+        span: "Women's Lower",
+        h5: "Flower print Night Trouser",
+        h4: "Rs 290/-",
+        itemCode: "f1",
+        prod: "womenLower"
+    },
+    {
+        imgsrc: "img/p (4).jpg",
+        alt: "product6",
+        span: "Women's Lower",
+        h5: "Check print Night Trouser",
+        h4: "Rs 290/-",
+        itemCode: "c3",
+        prod: "womenLower"
+    },
+    {
+        imgsrc: "img/p (5).jpg",
+        alt: "product6",
+        span: "Women's Lower",
+        h5: "Leopord print Night Trouser",
+        h4: "Rs 290/-",
+        itemCode: "l3",
+        prod: "womenLower"
+    },
+    {
+        imgsrc: "img/p (6).jpg",
+        alt: "product6",
+        span: "Women's Lower",
+        h5: "Leopord print Night Trouser",
+        h4: "Rs 290/-",
+        itemCode: "l4",
+        prod: "womenLower"
+    },
+    {
+        imgsrc: "img/p (7).jpg",
+        alt: "product6",
+        span: "Women's Lower",
+        h5: "Leopord print Night Trouser",
+        h4: "Rs 290/-",
+        itemCode: "l5",
+        prod: "womenLower"
+    },
+    {
+        imgsrc: "img/p (8).jpg",
+        alt: "product6",
+        span: "Women's Lower",
+        h5: "Leopord print Night Trouser",
+        h4: "Rs 290/-",
+        itemCode: "l6",
+        prod: "womenLower"
     }
+    
 ]
 
 
@@ -33,7 +103,7 @@ let n = arrdict.length;
 let productDisplay = () => {
     let html = '';
     for (let i = 0; i < n; i++) {
-        html += `<div class="pro">
+        html += `<div class="pro" onclick="window.location.href='singleProduct.html';">
             <img src="${arrdict[i].imgsrc}" alt="${arrdict[i].alt}">
             <div class="des">
                 <span>${arrdict[i].span}</span>
