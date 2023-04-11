2828ec96ff17600d25c1d1611c06b56971737f2b
Yash Dewan <yashdewan2580@gmail.com>
2023-04-11 21:50:20 +0530

    third commit

--- script.js
+++ script.js
@@ -1,123 +1,161 @@
 console.log("Supreme Collection - Shopping Website");
 
+
+
+
+// NAVBAR FOR MOBILE : 
+
+let mobbtn = document.getElementById('mobile');
+let crossbtn = document.getElementById('crossnav');
+
+mobbtn.addEventListener('click' , ()=>{
+    document.getElementById('nav').style.display="block";
+});
+crossbtn.addEventListener('click' , ()=>{
+    document.getElementById('nav').style.display="none";
+});
+
+
+
+
+
 // SHOP PAGE JS : 
 
+// product display : 
 let procon = document.getElementById('product-container-shopPage');
-
 const arrdict = [
     {
         imgsrc: "img/p1.jpg",
-        alt: "product1",
         span: "Women's Lower",
         h5: "Leapord print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "l1",
-        prod: "womenLower",
+        prod: "womenlower",
+        morePhotos: 1
+    },
+    {
+        imgsrc: "img/p (10).jpg",
+        span: "T-Shirt",
+        h5: "Peach Cotton TShirt",
+        h4: "Rs 160/-",
+        prod: "tshirt",
         morePhotos: 1
     },
     {
         imgsrc: "img/p2.jpg",
-        alt: "product2",
         span: "Women's Lower",
         h5: "Red Check print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "c1",
-        prod: "womenLower",
+        prod: "womenlower",
         morePhotos: 1
     },
     {
         imgsrc: "img/p (1).jpg",
-        alt: "product3",
         span: "Women's Lower",
         h5: "Leapord print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "l2",
-        prod: "womenLower",
+        prod: "womenlower",
         morePhotos: 1
     },
     {
         imgsrc: "img/p (2).jpg",
-        alt: "product4",
         span: "Women's Lower",
         h5: "Black Check print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "c2",
-        prod: "womenLower",
+        prod: "womenlower",
         morePhotos: 1
     },
     {
         imgsrc: "img/p (3).jpg",
-        alt: "product5",
         span: "Women's Lower",
         h5: "Flower print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "f1",
-        prod: "womenLower",
+        prod: "womenlower",
+        morePhotos: 1
+    },
+    {
+        imgsrc: "img/p (12).jpg",
+        span: "T-Shirt",
+        h5: "Mehroon Cotton TShirt",
+        h4: "Rs 160/-",
+        prod: "tshirt",
         morePhotos: 1
     },
     {
         imgsrc: "img/p (4).jpg",
-        alt: "product6",
         span: "Women's Lower",
         h5: "Check print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "c3",
-        prod: "womenLower",
+        prod: "womenlower",
         morePhotos: 1
     },
     {
         imgsrc: "img/p (5).jpg",
-        alt: "product6",
         span: "Women's Lower",
         h5: "Leopord print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "l3",
-        prod: "womenLower",
+        prod: "womenlower",
         morePhotos: 1
     },
     {
         imgsrc: "img/p (6).jpg",
-        alt: "product6",
         span: "Women's Lower",
         h5: "Leopord print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "l4",
-        prod: "womenLower",
+        prod: "womenlower",
         morePhotos: 1
     },
     {
         imgsrc: "img/p (7).jpg",
-        alt: "product6",
         span: "Women's Lower",
         h5: "Leopord print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "l5",
-        prod: "womenLower",
+        prod: "womenlower",
         morePhotos: 1
     },
     {
         imgsrc: "img/p (8).jpg",
-        alt: "product6",
         span: "Women's Lower",
         h5: "Leopord print Night Trouser",
         h4: "Rs 290/-",
-        itemCode: "l6",
-        prod: "womenLower",
+        prod: "womenlower",
         morePhotos: 1
-    }
-    
+    },
+    {
+        imgsrc: "img/p (15).jpg",
+        span: "T-Shirt",
+        h5: "Mustard  Cotton Tshirt",
+        h4: "Rs 160/-",
+        prod: "tshirt",
+        morePhotos: 1
+    },
+    {
+        imgsrc: "img/p (17).jpg",
+        span: "T-Shirt",
+        h5: "Red Cotton TShirt",
+        h4: "Rs 160/-",
+        prod: "tshirt",
+        morePhotos: 1
+    },
+    {
+        imgsrc: "img/p (18).jpg",
+        span: "T-Shirt",
+        h5: "Grey Cotton TShirt",
+        h4: "Rs 160/-",
+        prod: "tshirt",
+        morePhotos: 1
+    },
+
 ]
 
-
-let n = arrdict.length;
-let productDisplay = () => {
+let productDisplay = (prodsarr) => {
+    let n = prodsarr.length;
     let html = '';
     for (let i = 0; i < n; i++) {
-        html += `<div class="pro" onclick="window.location.href='singleProduct.html';">
-            <img src="${arrdict[i].imgsrc}" alt="${arrdict[i].alt}">
+        html += `<div id="prod${i+1}" class="pro ${prodsarr[i].prod}" onclick="window.location.href='singleProduct.html';">
+            <img src="${prodsarr[i].imgsrc}" alt="Product Image">
             <div class="des">
-                <span>${arrdict[i].span}</span>
-                <h5>${arrdict[i].h5}</h5>
+                <span>${prodsarr[i].span}</span>
+                <h5>${prodsarr[i].h5}</h5>
                 <div class="star">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -125,12 +163,30 @@ let productDisplay = () => {
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
                 </div>
-                <h4>${arrdict[i].h4}</h4>
+                <h4>${prodsarr[i].h4}</h4>
             </div>
             <a href="#"><i class="fas fa-shopping-bag cart"></i></a>
             </div>`;
     }
     procon.innerHTML = html;
 }
+productDisplay(arrdict);
 
-productDisplay();
+// filter : 
+let filterval = document.getElementById('filterShopid');
+
+filterval.addEventListener('click' , ()=>{
+    console.log(filterval.value);
+    if(filterval.value == "all"){
+        productDisplay(arrdict);
+    }else{
+        const newArr = arrdict.filter((e)=>{
+            return (filterval.value == e.prod);
+        });
+        productDisplay(newArr);
+    }
+});
+
+// small image showing :
+
+
